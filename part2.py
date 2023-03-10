eb89a76ab06c3502b22e0d760d7fc38b5029a3cb
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2023-03-10 09:13:54 -0700

    Update part2.py

--- part2.py
+++ part2.py
@@ -1,53 +1,38 @@
-# Import necessary libraries
+#part 2
+
+
+
+import requests
 import pandas as pd
-from pytrends.request import TrendReq
 
-# Define function to get Google Trends data
-def get_google_trends_data(keyword, timeframe='today 5-y', geo='US'):
-    pytrends = TrendReq()
-    pytrends.build_payload([keyword], cat=0, timeframe=timeframe, geo=geo, gprop='')
-    data = pytrends.interest_over_time()[[keyword]]
-    data.drop(labels=['isPartial'], axis=1, inplace=True)
-    return data
+def get_btc_price_data():
+    url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=90"
+    response = requests.get(url)
+    if response.ok:
+        data = response.json()
+        prices = data.get("prices", [])
+        df = pd.DataFrame(prices, columns=["timestamp", "price"])
+        df["timestamp"] = pd.to_datetime(df["timestamp"], unit="ms")
+        df = df.set_index("timestamp")
+        return df
+    else:
+        return pd.DataFrame()
 
-# Define function to preprocess the data
-def preprocess_data(data):
-    # Drop rows with missing values
-    data = data.dropna()
-    # Normalize the data
-    data = (data - data.mean()) / data.std()
-    # Add a column of 1s to serve as the intercept term
-    data['intercept'] = 1
-    # Rearrange columns so that intercept column is first
-    cols = data.columns.tolist()
-    cols = cols[-1:] + cols[:-1]
-    data = data[cols]
-    return data
+def market_sentiment_score():
+    # Fetch historical price data for Bitcoin
+    price_data = get_btc_price_data()
 
-# Define function to perform linear regression
-def linear_regression(data):
-    X = data.iloc[:, :-1]
-    y = data.iloc[:, -1]
-    beta = pd.Series([0] * X.shape[1])
-    while True:
-        residuals = y - X.dot(beta)
-        gradient = -2 * X.T.dot(residuals)
-        if gradient.abs().max() < 0.0001:
-            break
-        else:
-            beta = beta - 0.00001 * gradient
-    return beta
+    if not price_data.empty:
+        # Calculate the RSI for Bitcoin
+        delta = price_data['price'].diff()
+        gain = delta.where(delta > 0, 0)
+        loss = -delta.where(delta < 0, 0)
+        avg_gain = gain.rolling(window=12).mean()
+        avg_loss = loss.rolling(window=12).mean()
+        rs = avg_gain / avg_loss
+        rsi = 100 - (100 / (1 + rs))
 
-# Get Google Trends data for Bitcoin
-data = get_google_trends_data('Bitcoin')
-
-# Preprocess the data
-data = preprocess_data(data)
-
-# Perform linear regression on the preprocessed data
-beta = linear_regression(data)
-
-# Print the coefficients
-print("The coefficients are: ")
-print(beta)
-
+        # Return the current sentiment score based on the RSI
+        return rsi.iloc[-1]
+    else:
+        return None
