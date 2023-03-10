5226cc73edca78899563e75d1fd4b14176d9f02b
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2023-03-10 08:28:38 -0700

    Update part3.py

--- part3.py
+++ part3.py
@@ -1,24 +1,14 @@
+# Import necessary libraries and functions from parts 1 and 2
 import pandas as pd
-import requests
-from bs4 import BeautifulSoup
+from part1 import get_fed_statements
+from part2 import market_sentiment_score
 
-# Define statements as a global variable
-statements = []
-
-def fetch_fed_statements():
-    global statements
-    # ... code to fetch and process statements ...
-
-
-def market_sentiment_score():
-    # ... code to analyze sentiment and calculate accuracy ...
-
-
-  if __name__ == "__main__":
-    #df
-    fed_df = fetch_fed_statements()
+# Part 3
+if __name__ == "__main__":
+    # Run part 1
+    fed_df = get_fed_statements()
     print("Federal Reserve Statements:\n", fed_df.head(), "\n")
 
-    #acc
+    # Run part 2
     acc = market_sentiment_score()
     print("Accuracy:", acc)
