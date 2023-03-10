124b000c2b28d7ff2979f03795d2e1fb50e9c7aa
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2023-03-10 09:54:42 -0700

    Update part2.py

--- part2.py
+++ part2.py
@@ -1,16 +1,30 @@
 #part 2
 
 
-import yfinance as yf
+# Import necessary packages and modules
+from transformers import BertTokenizer, BertForSequenceClassification
+import torch
 import pandas as pd
 from ta.momentum import RSIIndicator
 
-def get_bitcoin_data():
-    data = yf.download("BTC-USD", start="2010-01-01", end="2022-01-01")
-    # Calculate RSI
-    indicator = RSIIndicator(data['Close'], window=14)
-    data['RSI'] = indicator.rsi()
-    # Drop rows with missing values
-    data = data.dropna()
-    return data
+# Load the pre-trained BERT model and tokenizer
+model = BertForSequenceClassification.from_pretrained("bert-base-uncased")
+tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 
+def analyze_sentiment(statements):
+    # Analyze the sentiment of the statements using BERT
+    scores = []
+    for statement in statements:
+        input_ids = torch.tensor([tokenizer.encode(statement, add_special_tokens=True)])
+        with torch.no_grad():
+            output = model(input_ids)[0]
+        score = torch.sigmoid(output[0,0]).item()
+        scores.append(score)
+    return scores
+
+def calculate_rsi(data):
+    # Add RSI column
+    close = pd.DataFrame(data)['close']
+    rsi_indicator = RSIIndicator(close, window=14)
+    rsi = rsi_indicator.rsi()
+    return rsi
