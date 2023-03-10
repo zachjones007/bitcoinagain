22658c4775552290325247cd9a9a5f8a43c48706
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2023-03-10 11:56:55 -0700

    Update part3.py

--- part3.py
+++ part3.py
@@ -1,79 +1,24 @@
 #part 3
+from part1 import analyze_federal_reserve
+from part2 import get_bitcoin_data, analyze_bitcoin
 
+def main():
+    # Get data from APIs
+    bitcoin_data = get_bitcoin_data()
+    federal_reserve_data = analyze_federal_reserve()
 
+    # Analyze data
+    bitcoin_analysis = analyze_bitcoin(bitcoin_data)
+    federal_reserve_analysis = analyze_federal_reserve(federal_reserve_data)
 
-# Import necessary packages and modules
-from part2 import get_bitcoin_data
-from transformers import BertTokenizer, BertForSequenceClassification
-import torch
-import yfinance as yf
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+    # Determine overall market sentiment
+    sentiment = bitcoin_analysis + federal_reserve_analysis
+    if sentiment > 0:
+        print("Market sentiment is bullish.")
+    elif sentiment < 0:
+        print("Market sentiment is bearish.")
+    else:
+        print("Market sentiment is neutral.")
 
-# Load the pre-trained BERT model and tokenizer
-model = BertForSequenceClassification.from_pretrained("bert-base-uncased")
-tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
-
-# Fetch historical data for Bitcoin
-data = get_bitcoin_data()
-
-if not data.empty:
-    # Analyze the sentiment of the statements made by the Federal Reserve
-    statements = ["The Federal Reserve is committed to maintaining price stability and supporting the economic recovery.", "The Federal Reserve is concerned about rising inflation and may raise interest rates."]
-    scores = []
-    for statement in statements:
-        input_ids = torch.tensor([tokenizer.encode(statement, add_special_tokens=True)])
-        with torch.no_grad():
-            output = model(input_ids)[0]
-        score = torch.sigmoid(output[0,0]).item()
-        scores.append(score)
-
-    # Add RSI column
-    delta = data['Close'].diff()
-    gain = delta.where(delta > 0, 0)
-    loss = -delta.where(delta < 0, 0)
-    avg_gain = gain.rolling(window=12).mean()
-    avg_loss = loss.rolling(window=12).mean()
-    rs = avg_gain / avg_loss
-    data['RSI'] = 100 - (100 / (1 + rs))
-
-    # Assign sentiment scores to the historical data DataFrame
-    data['sentiment'] = [scores[i % len(scores)] for i in range(len(data))]
-
-    # Create a new column for the label, 1 for an increase in price and 0 for a decrease
-    data['label'] = (data['Close'] > data['Close'].shift(1)).astype(int)
-
-    # Create a feature dataset, dropping unnecessary columns
-    features = data[['RSI','sentiment']]
-
-    # Fill missing values with mean
-    features = features.fillna(features.mean())
-
-    # Drop rows with missing values
-    features = features.dropna()
-
-    # Split the dataset into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(features, data['label'], test_size=0.2, random_state=42)
-
-    # Create a logistic regression model
-    log_reg = LogisticRegression()
-
-    # Train the model on the training data
-    log_reg.fit(X_train, y_train)
-
-    # Test the model's performance on the testing data
-    accuracy = log_reg.score(X_test, y_test)
-
-    # Predict the next day's label using the logistic regression model
-    last_row = data.iloc[-1]
-    next_day_features = [last_row['RSI'], scores[-1]]
-    next_day_features = pd.DataFrame([next_day_features], columns=['RSI', 'sentiment'])
-    next_day_features = next_day_features.fillna(features.mean())
-    next_day_label = log_reg.predict(next_day_features)[0]
-
-    # Determine market outlook based on predicted label
-    market_outlook = "Bearish" if next_day_label == 0 else "Bullish"
-    print("The market outlook for Bitcoin is:", market_outlook)
-else:
-    print("Failed to fetch Bitcoin data")
+if __name__ == "__main__":
+    main()1
