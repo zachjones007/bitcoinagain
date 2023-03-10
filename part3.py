37fa36fc8a1d763911281a03ace76ed9c1ef0305
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2023-03-10 09:27:49 -0700

    Update part3.py

--- part3.py
+++ part3.py
@@ -1,15 +1,65 @@
-#part 3
-
-
-
-
-from part1 import get_fed_statements
 from part2 import get_bitcoin_data
 
-# Fetch the Federal Reserve statements and print them out
-statements = get_fed_statements()
-print("Federal Reserve Statements:\n", statements, "\n")
+# Load the pre-trained BERT model and tokenizer
+print("Loading BERT model and tokenizer...")
+# Code for loading BERT model and tokenizer goes here
 
-# Fetch Bitcoin data and print it out
-bitcoin_data = get_bitcoin_data()
-print("Bitcoin Data:\n", bitcoin_data.head(), "\n")
+# Fetch historical data for Bitcoin
+print("Fetching Bitcoin data...")
+data = get_bitcoin_data()
+
+if not data.empty:
+    # Analyze the sentiment of the statements made by the Federal Reserve
+    print("Analyzing sentiment of Federal Reserve statements...")
+    # Code for analyzing sentiment goes here
+
+    # Add RSI column
+    print("Calculating RSI...")
+    # Code for calculating RSI goes here
+
+    # Assign sentiment scores to the historical data DataFrame
+    print("Assigning sentiment scores to data...")
+    # Code for assigning sentiment scores goes here
+
+    # Create a new column for the label, 1 for an increase in price and 0 for a decrease
+    print("Creating label column...")
+    # Code for creating label column goes here
+
+    # Create a feature dataset, dropping unnecessary columns
+    print("Creating feature dataset...")
+    # Code for creating feature dataset goes here
+
+    # Fill missing values with mean
+    print("Filling missing values with mean...")
+    # Code for filling missing values goes here
+
+    # Drop rows with missing values
+    print("Dropping rows with missing values...")
+    # Code for dropping rows with missing values goes here
+
+    # Split the dataset into training and testing sets
+    print("Splitting dataset into training and testing sets...")
+    # Code for splitting dataset goes here
+
+    # Create a logistic regression model
+    print("Creating logistic regression model...")
+    # Code for creating logistic regression model goes here
+
+    # Train the model on the training data
+    print("Training logistic regression model...")
+    # Code for training logistic regression model goes here
+
+    # Test the model's performance on the testing data
+    print("Testing logistic regression model...")
+    # Code for testing logistic regression model goes here
+
+    # Predict the next day's label using the logistic regression model
+    print("Predicting next day's label...")
+    # Code for predicting next day's label goes here
+
+    # Print whether the market is bullish or bearish based on the predicted label
+    print("Determining market outlook based on predicted label...")
+    # Code for determining market outlook goes here
+
+else:
+    print("The historical data is empty.")
