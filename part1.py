2b18743258731f889f5a8e77d6c95fa546b24af5
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2023-03-10 08:32:24 -0700

    Update part1.py

--- part1.py
+++ part1.py
@@ -1,27 +1,17 @@
-def get_fed_statements():
-    # Fetch the webpage containing the list of recent FOMC statements
-    url = 'https://www.federalreserve.gov/monetarypolicy/fomccalendars.htm'
-    response = requests.get(url)
-
-    # Parse the HTML content of the webpage
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    # Extract the URLs for the statements
-    statement_links = []
-    for link in soup.find_all('a', href=True):
-        href = link['href']
-        if href.startswith('/newsevents/pressreleases/monetary20'):
-            statement_links.append('https://www.federalreserve.gov' + href)
+# Part 1
+def fetch_fed_statements():
+    global statements
+    # ... code to fetch and process statements ...
 
     # Fetch the content of the statements
     statements = []
     for link in statement_links:
         response = requests.get(link)
         soup = BeautifulSoup(response.content, 'html.parser')
-        content_div = soup.find('div', class_='col-xs-12 col-sm-8 col-md-8')
-        if content_div is not None:
-            content = content_div.get_text().strip()
-            statements.append(content)
+        content = soup.find('div', class_='col-xs-12 col-sm-8 col-md-8')
+        if content is not None:
+            content = content.get_text().strip()
+        statements.append(content)
 
     # Create a Pandas DataFrame containing the statements
     data = {'text': statements}
