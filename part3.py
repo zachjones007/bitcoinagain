22da010507c6339ec34c56b7af70e53289526916
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2023-03-10 08:53:35 -0700

    Update part3.py

--- part3.py
+++ part3.py
@@ -1,29 +1,20 @@
-# Import necessary libraries
-import pandas as pd
-import requests
-from bs4 import BeautifulSoup
-import re
+# Define statements as a global variable
+statements = []
 
-# Define a function to get the text of recent Federal Reserve statements
-def get_fed_statements():
-    # Send a request to the Federal Reserve's press release page
-    url = 'https://www.federalreserve.gov/newsevents/pressreleases.htm'
-    response = requests.get(url)
-    
-    # Parse the HTML content using BeautifulSoup
-    soup = BeautifulSoup(response.content, 'html.parser')
-    
-    # Find the link to the most recent press release
-    latest_link = soup.find('a', string=re.compile('Press Release')).get('href')
-    
-    # Send a request to the press release page and get the content
-    response = requests.get(latest_link)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    
-    # Extract the content of the press release
-    content = soup.find('div', class_='col-xs-12 col-sm-8 col-md-8').get_text().strip()
-    
-    return content
+def fetch_fed_statements():
+    global statements
+    # ... code to fetch and process statements ...
 
-# Print the text of the most recent Federal Reserve statement
-print(get_fed_statements())
+
+def market_sentiment_score():
+    # ... code to analyze sentiment and calculate accuracy ...
+
+
+  if __name__ == "__main__":
+    #df
+    fed_df = fetch_fed_statements()
+    print("Federal Reserve Statements:\n", fed_df.head(), "\n")
+
+    #acc
+    acc = market_sentiment_score()
+    print("Accuracy:", acc)
