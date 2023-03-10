af7297c2ed5ebe31b94adb4f0c06c60755e9a4ad
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2023-03-10 11:54:13 -0700

    Update part1.py

--- part1.py
+++ part1.py
@@ -1,40 +1,25 @@
-import requests
-import json
-from datetime import datetime
+#part 1 
 
-def get_bart_data():
-    url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
-    response = requests.get(url)
-    data = json.loads(response.text)
-    rate = data['bpi']['USD']['rate']
-    return float(rate.replace(',', ''))
+import requests
+from bs4 import BeautifulSoup
 
 def get_federal_reserve_data():
-    url = 'https://www.federalreserve.gov/releases/h3/current/h3.htm'
+    url = "https://www.federalreserve.gov/newsevents/speeches.htm"
     response = requests.get(url)
-    table_start = '<table cellpadding="3" cellspacing="0" width="100%">'
-    table_end = '</table>'
-    table_data = response.text.split(table_start)[1].split(table_end)[0]
-    row_start = '<tr>'
-    row_end = '</tr>'
-    cell_start = '<td'
-    cell_end = '</td>'
-    rows = table_data.split(row_start)[1:]
-    last_row = rows[-1]
-    cells = last_row.split(cell_start)[1:]
-    interest_rate = cells[1].split(cell_end)[0]
-    return float(interest_rate)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    speeches = soup.find_all('div', class_='col-xs-12 col-sm-6 col-md-4')
+    return speeches
 
-def predict_market():
-    bart = get_bart_data()
-    interest_rate = get_federal_reserve_data()
-    if bart > 50000 and interest_rate < 0.25:
-        return 'Bullish'
+def analyze_federal_reserve(speeches):
+    keywords = ['dovish', 'hawkish']
+    speech_text = ''
+    for speech in speeches:
+        speech_text += speech.text.lower()
+    if any(keyword in speech_text for keyword in keywords):
+        if 'dovish' in speech_text:
+            return -1 # bearish
+        else:
+            return 1 # bullish
     else:
-        return 'Bearish'
-
-if __name__ == '__main__':
-    market_prediction = predict_market()
-    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    print(f'Market prediction at {current_time}: {market_prediction}')
+        return 0 # neutral
 
