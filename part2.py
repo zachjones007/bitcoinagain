ac7da4acca3fed0862b57445ad756cd06adbc047
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2023-03-10 09:40:04 -0700

    Update part2.py

--- part2.py
+++ part2.py
@@ -3,36 +3,14 @@
 
 import yfinance as yf
 import pandas as pd
-import numpy as np
+from ta.momentum import RSIIndicator
 
 def get_bitcoin_data():
-    """
-    Fetches historical data for Bitcoin from Yahoo Finance and returns it as a pandas DataFrame.
-    """
-    # Create a Ticker object for Bitcoin
-    btc = yf.Ticker("BTC-USD")
-
-    # Fetch historical data
-    data = btc.history(period="max")
-
+    data = yf.download("BTC-USD", start="2010-01-01", end="2022-01-01")
+    # Calculate RSI
+    indicator = RSIIndicator(data['Close'], window=14)
+    data['RSI'] = indicator.rsi()
+    # Drop rows with missing values
+    data = data.dropna()
     return data
 
-def calculate_rsi(data):
-    """
-    Calculates the Relative Strength Index (RSI) for the given Bitcoin data and returns it as a pandas DataFrame.
-    """
-    delta = data['Close'].diff()
-    gain = delta.where(delta > 0, 0)
-    loss = -delta.where(delta < 0, 0)
-    avg_gain = gain.rolling(window=12).mean()
-    avg_loss = loss.rolling(window=12).mean()
-    rs = avg_gain / avg_loss
-    rsi = 100 - (100 / (1 + rs))
-    return rsi.to_frame()
-
-def calculate_label(data):
-    """
-    Calculates the label (0 or 1) for each row in the given Bitcoin data and returns it as a pandas DataFrame.
-    """
-    label = (data['Close'] > data['Close'].shift(1)).astype(int)
-    return label.to_frame()
