46accf82dd946109ba7bd83293b517177fe23f95
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2023-03-10 14:13:40 -0700

    Update part2.py

--- part2.py
+++ part2.py
@@ -3,21 +3,22 @@
 import yfinance as yf
 import pandas as pd
 
-# Fetch historical data for Bitcoin
-btc = yf.Ticker("BTC-USD")
-data = btc.history(period="max")
+def calculate_rsi():
+    # Fetch historical data for Bitcoin
+    btc = yf.Ticker("BTC-USD")
+    data = btc.history(period="max")
 
-# Calculate RSI
-delta = data['Close'].diff()
-gain = delta.where(delta > 0, 0)
-loss = -delta.where(delta < 0, 0)
-avg_gain = gain.rolling(window=14).mean()
-avg_loss = loss.rolling(window=14).mean()
-rs = avg_gain / avg_loss
-rsi = 100 - (100 / (1 + rs))
+    # Calculate RSI
+    delta = data['Close'].diff()
+    gain = delta.where(delta > 0, 0)
+    loss = -delta.where(delta < 0, 0)
+    avg_gain = gain.rolling(window=14).mean()
+    avg_loss = loss.rolling(window=14).mean()
+    rs = avg_gain / avg_loss
+    rsi = 100 - (100 / (1 + rs))
 
-# Calculate score on a scale of -50 to 50
-score = ((rsi.iloc[-1] - 50) / 50) * 100
+    # Calculate score on a scale of -50 to 50
+    score = ((rsi.iloc[-1] - 50) / 50) * 100
 
-# Output single score on a scale of -50 to 50
-print(score)
+    # Output single score on a scale of -50 to 50
+    return score
