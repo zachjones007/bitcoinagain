7cdf7bad6da8a7b8e50bd561f7b2f0403298e209
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2023-03-10 08:31:23 -0700

    Update part1.py

--- part1.py
+++ part1.py
@@ -1,8 +1,3 @@
-# Part 1
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
-
 def get_fed_statements():
     # Fetch the webpage containing the list of recent FOMC statements
     url = 'https://www.federalreserve.gov/monetarypolicy/fomccalendars.htm'
@@ -23,11 +18,14 @@ def get_fed_statements():
     for link in statement_links:
         response = requests.get(link)
         soup = BeautifulSoup(response.content, 'html.parser')
-        content = soup.find('div', class_='col-xs-12 col-sm-8 col-md-8').get_text().strip()
-        statements.append(content)
+        content_div = soup.find('div', class_='col-xs-12 col-sm-8 col-md-8')
+        if content_div is not None:
+            content = content_div.get_text().strip()
+            statements.append(content)
 
     # Create a Pandas DataFrame containing the statements
     data = {'text': statements}
     df = pd.DataFrame(data)
 
     return df
+
