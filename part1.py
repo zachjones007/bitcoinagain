bd0f18a69394918ca2455cba770cbe1327d2518a
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2023-03-10 09:39:41 -0700

    Update part1.py

--- part1.py
+++ part1.py
@@ -1,35 +1,8 @@
-#part 1 
-
 import requests
-import json
 
-def get_fed_statements(api_key):
-    url = "https://api.thomsonreuters.com/permid/calais"
-    headers = {
-        "Content-Type": "text/raw",
-        "X-AG-Access-Token": api_key,
-        "outputformat": "application/json"
-    }
-
-    # Fetch the latest news articles from Reuters
-    response = requests.get("https://www.reuters.com", headers=headers)
-    articles = []
-    if response.ok:
-        articles = response.json().get("value", [])
-
-    # Extract statements from the Reuters articles using the Thomson Reuters API
-    statements = []
-    for article in articles:
-        if article.get("_type", "") == "Article":
-            content = article.get("body", "")
-            if content:
-                payload = content.encode("utf-8")
-                response = requests.post(url, headers=headers, data=payload)
-                if response.ok:
-                    data = response.json()
-                    for key in data:
-                        if "http://d.opencalais.com/persorg" in key and "policy" in data[key]:
-                            statements.append(data[key]["policy"])
-
-    return statements
+def get_bitcoin_price():
+    response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+    data = response.json()
+    price = data["bpi"]["USD"]["rate"]
+    return price
 
