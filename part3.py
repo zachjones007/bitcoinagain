be9d01797a1b7b6547d0c2fdd75ca065bbb1ba5c
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2023-03-10 08:25:26 -0700

    Update part3.py

--- part3.py
+++ part3.py
@@ -5,21 +5,20 @@ from bs4 import BeautifulSoup
 # Define statements as a global variable
 statements = []
 
-# Part 1
 def fetch_fed_statements():
     global statements
     # ... code to fetch and process statements ...
 
-# Part 2
+
 def market_sentiment_score():
     # ... code to analyze sentiment and calculate accuracy ...
 
-# Part 3
+
   if __name__ == "__main__":
-    # Run part 1
+    #df
     fed_df = fetch_fed_statements()
     print("Federal Reserve Statements:\n", fed_df.head(), "\n")
 
-    # Run part 2
+    #acc
     acc = market_sentiment_score()
     print("Accuracy:", acc)
