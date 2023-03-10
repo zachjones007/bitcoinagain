29ecc4dd9ded05e2791cf930bc3a9323e46fc10c
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2023-03-10 12:16:52 -0700

    Update part2.py

--- part2.py
+++ part2.py
@@ -1,30 +1,26 @@
 #part 2
 
-
-# Import necessary packages and modules
-from transformers import BertTokenizer, BertForSequenceClassification
-import torch
+import yfinance as yf
 import pandas as pd
-from ta.momentum import RSIIndicator
 
-# Load the pre-trained BERT model and tokenizer
-model = BertForSequenceClassification.from_pretrained("bert-base-uncased")
-tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+# Fetch historical data for Bitcoin
+btc = yf.Ticker("BTC-USD")
+data = btc.history(period="max")
 
-def analyze_sentiment(statements):
-    # Analyze the sentiment of the statements using BERT
-    scores = []
-    for statement in statements:
-        input_ids = torch.tensor([tokenizer.encode(statement, add_special_tokens=True)])
-        with torch.no_grad():
-            output = model(input_ids)[0]
-        score = torch.sigmoid(output[0,0]).item()
-        scores.append(score)
-    return scores
+# Calculate RSI
+delta = data['Close'].diff()
+gain = delta.where(delta > 0, 0)
+loss = -delta.where(delta < 0, 0)
+avg_gain = gain.rolling(window=14).mean()
+avg_loss = loss.rolling(window=14).mean()
+rs = avg_gain / avg_loss
+rsi = 100 - (100 / (1 + rs))
 
-def calculate_rsi(data):
-    # Add RSI column
-    close = pd.DataFrame(data)['close']
-    rsi_indicator = RSIIndicator(close, window=14)
-    rsi = rsi_indicator.rsi()
-    return rsi
+# Calculate the most recent RSI value
+recent_rsi = rsi.iloc[-1]
+
+# Scale RSI to -50 to 50 range
+scaled_rsi = ((recent_rsi - 50) / 50) * 100
+
+# Print the scaled RSI value
+print("Scaled RSI: ", scaled_rsi)
