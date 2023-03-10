815bf33f49bffa2ad83a2b229f7123c02f75ae29
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2023-03-10 09:28:38 -0700

    Update part2.py

--- part2.py
+++ part2.py
@@ -1,38 +1,38 @@
 #part 2
 
 
-
-import requests
+import yfinance as yf
 import pandas as pd
+import numpy as np
 
-def get_btc_price_data():
-    url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=90"
-    response = requests.get(url)
-    if response.ok:
-        data = response.json()
-        prices = data.get("prices", [])
-        df = pd.DataFrame(prices, columns=["timestamp", "price"])
-        df["timestamp"] = pd.to_datetime(df["timestamp"], unit="ms")
-        df = df.set_index("timestamp")
-        return df
-    else:
-        return pd.DataFrame()
+def get_bitcoin_data():
+    """
+    Fetches historical data for Bitcoin from Yahoo Finance and returns it as a pandas DataFrame.
+    """
+    # Create a Ticker object for Bitcoin
+    btc = yf.Ticker("BTC-USD")
 
-def market_sentiment_score():
-    # Fetch historical price data for Bitcoin
-    price_data = get_btc_price_data()
+    # Fetch historical data
+    data = btc.history(period="max")
 
-    if not price_data.empty:
-        # Calculate the RSI for Bitcoin
-        delta = price_data['price'].diff()
-        gain = delta.where(delta > 0, 0)
-        loss = -delta.where(delta < 0, 0)
-        avg_gain = gain.rolling(window=12).mean()
-        avg_loss = loss.rolling(window=12).mean()
-        rs = avg_gain / avg_loss
-        rsi = 100 - (100 / (1 + rs))
+    return data
 
-        # Return the current sentiment score based on the RSI
-        return rsi.iloc[-1]
-    else:
-        return None
+def calculate_rsi(data):
+    """
+    Calculates the Relative Strength Index (RSI) for the given Bitcoin data and returns it as a pandas DataFrame.
+    """
+    delta = data['Close'].diff()
+    gain = delta.where(delta > 0, 0)
+    loss = -delta.where(delta < 0, 0)
+    avg_gain = gain.rolling(window=12).mean()
+    avg_loss = loss.rolling(window=12).mean()
+    rs = avg_gain / avg_loss
+    rsi = 100 - (100 / (1 + rs))
+    return rsi.to_frame()
+
+def calculate_label(data):
+    """
+    Calculates the label (0 or 1) for each row in the given Bitcoin data and returns it as a pandas DataFrame.
+    """
+    label = (data['Close'] > data['Close'].shift(1)).astype(int)
+    return label.to_frame()
