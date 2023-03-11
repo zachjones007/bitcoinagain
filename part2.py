a037aa3902c6e982028a5d89e6b74783a4365042
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2023-03-10 18:16:57 -0700

    Update part2.py

--- part2.py
+++ part2.py
@@ -1,24 +1,44 @@
 #part 2
+import requests
+import numpy as np
 
-import yfinance as yf
-import pandas as pd
+ENDPOINT = "https://api.pro.coinbase.com"
+PRODUCT_ID = "BTC-USD"
 
-def calculate_rsi():
-    # Fetch historical data for Bitcoin
-    btc = yf.Ticker("BTC-USD")
-    data = btc.history(period="max")
+def get_price():
+    url = f"{ENDPOINT}/products/{PRODUCT_ID}/ticker"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        price = float(data["price"])
+        return price
+    else:
+        raise Exception(f"Error retrieving price: {response.status_code} - {response.text}")
 
-    # Calculate RSI
-    delta = data['Close'].diff()
-    gain = delta.where(delta > 0, 0)
-    loss = -delta.where(delta < 0, 0)
-    avg_gain = gain.rolling(window=14).mean()
-    avg_loss = loss.rolling(window=14).mean()
-    rs = avg_gain / avg_loss
-    rsi = 100 - (100 / (1 + rs))
+def calculate_rsi(prices, period=14):
+    changes = np.diff(prices)
+    gains = changes.clip(min=0)
+    losses = -changes.clip(max=0)
+    avg_gain = np.mean(gains[-period:])
+    avg_loss = np.mean(losses[-period:])
+    if avg_loss == 0:
+        rsi = 100
+    else:
+        rs = avg_gain / avg_loss
+        rsi = 100 - (100 / (1 + rs))
+    return rsi
 
-    # Calculate score on a scale of -50 to 50
-    score = ((rsi.iloc[-1] - 50) / 50) * 100
+def main():
+    prices = []
+    for i in range(15):
+        price = get_price()
+        prices.append(price)
+        print(f"BTC-USD Price: ${price:.2f}")
+    rsi = calculate_rsi(prices)
+    if np.isnan(rsi):
+        print("BTC-USD RSI: N/A")
+    else:
+        print(f"BTC-USD RSI: {rsi:.2f}")
 
-    # Output single score on a scale of -50 to 50
-    return score
+if __name__ == "__main__":
+    main()
