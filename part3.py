f9be2499d532a2027df88452ae95865bbc0f0faa
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2023-03-10 18:47:32 -0700

    Update part3.py

--- part3.py
+++ part3.py
@@ -5,3 +5,10 @@ def combine():
     sentiment = part1.bullishorbearish(-1)
     score = part2.calculate_rsi_score()
     return sentiment + score
+
+def main():
+    result = combine()
+    print(f"The combined score is {result}")
+
+if __name__ == "__main__":
+    main()
