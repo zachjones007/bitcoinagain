791cc21fc0d9fa42665a9f676e5aa85fc97d5fb3
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2023-03-10 18:50:18 -0700

    Update part3.py

--- part3.py
+++ part3.py
@@ -2,13 +2,13 @@ import part1
 import part2
 
 def combine():
-    sentiment = part1.bullishorbearish(-1)
+    sentiment = part1.determine_market_trend(-1)
     score = part2.calculate_rsi_score()
     return sentiment + score
 
 def main():
     result = combine()
-    print(f"The combined score is {result}")
+    print(f"The combined score is: {result}")
 
 if __name__ == "__main__":
     main()
