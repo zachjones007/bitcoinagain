9882f922554dd7a3eb4093a95dc02364a9f8246c
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2023-03-11 12:10:07 -0700

    Update part1.py

--- part1.py
+++ part1.py
@@ -1,28 +1,53 @@
 import requests
+import numpy as np
 
-def get_news_sentiment():
-    url = "https://newsapi.org/v2/everything?q=bitcoin&from=2022-02-08&sortBy=publishedAt&apiKey=YOUR_API_KEY"
-    response = requests.get(url)
-    if response.status_code == 200:
+class Bitcoin:
+    def __init__(self):
+        self.base_url = 'https://api.pro.coinbase.com'
+        self.product_id = 'BTC-USD'
+
+    def get_historical_data(self, start_time, end_time):
+        url = f"{self.base_url}/products/{self.product_id}/candles"
+        params = {
+            'start': start_time.isoformat(),
+            'end': end_time.isoformat(),
+            'granularity': '86400' # 1 day
+        }
+        response = requests.get(url, params=params)
+
         data = response.json()
-        articles = data["articles"]
-        total_articles = len(articles)
-        positive_articles = 0
-        for article in articles:
-            title = article["title"].lower()
-            if "bitcoin" in title and ("high" in title or "increase" in title or "rise" in title):
-                positive_articles += 1
-        positive_ratio = positive_articles / total_articles
-        if positive_ratio >= 0.5:
-            return 1
-        else:
-            return -1
-    else:
-        raise Exception(f"Error retrieving news: {response.status_code} - {response.text}")
+        prices = [float(row[4]) for row in data]
+        rsi = self.calculate_rsi(prices)
 
-def bullishorbearish():
-    sentiment = get_news_sentiment()
-    if sentiment == 1:
-        return "Bullish"
-    else:
-        return "Bearish"
+        return {
+            'prices': prices,
+            'rsi': rsi
+        }
+
+    def calculate_rsi(self, data, period=14):
+        # Calculate the gains and losses for each period
+        changes = [data[i] - data[i-1] for i in range(1, len(data))]
+        gains = [max(0, x) for x in changes]
+        losses = [max(0, -x) for x in changes]
+
+        # Calculate the average gains and losses over the specified period
+        avg_gain = sum(gains[:period]) / period
+        avg_loss = sum(losses[:period]) / period
+
+        # Calculate the initial RSI value
+        rs = avg_gain / avg_loss
+        rsi = 100 - (100 / (1 + rs))
+
+        # Calculate the RSI values for subsequent periods
+        for i in range(period, len(data)):
+            change = data[i] - data[i-1]
+            gain = max(0, change)
+            loss = max(0, -change)
+
+            avg_gain = ((period - 1) * avg_gain + gain) / period
+            avg_loss = ((period - 1) * avg_loss + loss) / period
+
+            rs = avg_gain / avg_loss
+            rsi = 100 - (100 / (1 + rs))
+
+        return rsi
