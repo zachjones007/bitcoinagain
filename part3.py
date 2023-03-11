c7aeec1e70b73ec59a390f2233608fcb49f8501d
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2023-03-10 18:44:22 -0700

    Update part3.py

--- part3.py
+++ part3.py
@@ -1,8 +1,7 @@
 import part1
 import part2
 
-def calculate_combined_score():
-    sentiment = 1 if part1.bullishorbearish() == "Bullish" else -1
+def combine():
+    sentiment = part1.bullishorbearish(-1)
     score = part2.calculate_rsi_score()
-    return sentiment * score
-
+    return sentiment + score
