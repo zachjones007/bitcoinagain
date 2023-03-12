89463d40ddae8b75d1f98af4858b4234ed32a9aa
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2023-03-12 11:07:18 -0700

    Update part1.py

--- part1.py
+++ part1.py
@@ -19,8 +19,37 @@ def get_rsi(symbol, interval, time_period):
     rsi = 100 - (100 / (1 + rs))
     return rsi.iloc[-1]
 
+def get_overbought_oversold(symbol, interval):
+    url = f'https://api.binance.com/api/v3/klines?symbol={symbol}&interval={interval}'
+    response = requests.get(url)
+    data = response.json()
+    close_prices = [float(entry[4]) for entry in data]
+    high_prices = [float(entry[2]) for entry in data]
+    low_prices = [float(entry[3]) for entry in data]
+    for i in range(len(close_prices)):
+        if high_prices[i] > 0:
+            rsi = 100 - (100 / (1 + (close_prices[i] / high_prices[i])))
+            break
+        elif low_prices[i] > 0:
+            rsi = 100 - (100 / (1 + (close_prices[i] / low_prices[i])))
+            break
+    overbought = (rsi >= 70)
+    oversold = (rsi <= 30)
+    return overbought, oversold
+
+def get_market_sentiment(symbol, interval='1d', rsi_time_period=14):
+    rsi_value = get_rsi(symbol, interval, rsi_time_period)
+    overbought, oversold = get_overbought_oversold(symbol, interval)
+    if overbought:
+        sentiment = 100
+    elif oversold:
+        sentiment = 0
+    else:
+        sentiment = (rsi_value - 30) * 100 / 40
+    return sentiment
+
 symbol = 'BTCUSDT'
 interval = '1d'
-time_period = 14
-rsi_value = get_rsi(symbol, interval, time_period)
-print('RSI:', rsi_value)
+rsi_time_period = 14
+market_sentiment = get_market_sentiment(symbol, interval, rsi_time_period)
+print('Market Sentiment:', market_sentiment)
