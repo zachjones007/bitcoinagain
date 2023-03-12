c78a859cdf8a3b8f536cf1d6d55bbe90eafee77e
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2023-03-12 13:10:14 -0700

    Update part1.py

--- part1.py
+++ part1.py
@@ -1,42 +1,20 @@
 import requests
 
-def get_market_sentiment(symbol, interval, rsi_time_period):
-    url = f"https://api.coingecko.com/api/v3/coins/{symbol}/market_chart?vs_currency=usd&days=30&interval={interval}"
+def get_price():
+    url = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd'
     response = requests.get(url)
     data = response.json()
+    return data['bitcoin']['usd']
 
-    prices = []
-    for p in data['prices']:
-        prices.append(p[1])
+def get_volume():
+    url = 'https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=1'
+    response = requests.get(url)
+    data = response.json()
+    volume = 0
+    for d in data['total_volumes']:
+        volume += d[1]
+    return volume
 
-    changes = []
-    for i in range(1, len(prices)):
-        changes.append(prices[i] - prices[i-1])
-
-    ups = 0
-    downs = 0
-    for c in changes[:rsi_time_period]:
-        if c > 0:
-            ups += c
-        else:
-            downs -= c
-
-    avg_up = ups / rsi_time_period
-    avg_down = downs / rsi_time_period
-    rs = avg_up / avg_down
-    rsi_value = 100 - (100 / (1 + rs))
-
-    if rsi_value < 30:
-        sentiment = 'Bearish'
-    elif rsi_value > 70:
-        sentiment = 'Bullish'
-    else:
-        sentiment = 'Neutral'
-
-    volume_url = f"https://api.coingecko.com/api/v3/coins/{symbol}"
-    volume_response = requests.get(volume_url)
-    volume_data = volume_response.json()
-    market_data = volume_data['market_data']
-    volume_score = market_data['total_volume']['usd'] / market_data['market_cap']['usd']
-
-    return sentiment, volume_score
+if __name__ == '__main__':
+    print(f"Bitcoin price: {get_price()}")
+    print(f"Bitcoin volume: {get_volume()}")
