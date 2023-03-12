f0eac52cf3fb4941f7b9ccbf881bf5dd7ab9fbfc
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2023-03-12 12:28:09 -0700

    Update print.py

--- print.py
+++ print.py
@@ -1,23 +1,22 @@
-import os
-import sys
-
-# add the bitcoinagain directory to the path
-module_path = os.path.abspath(os.path.join('..'))
-if module_path not in sys.path:
-    sys.path.append(module_path)
-
 from bitcoinagain.part1 import get_rsi
 from bitcoinagain.part2 import get_market_trend
 from bitcoinagain.part3 import get_market_sentiment
 
-def print_results(symbol='BTCUSDT', interval='1d', rsi_time_period=14):
-    rsi_value = get_rsi(symbol, interval, rsi_time_period)
-    market_trend = get_market_trend(symbol, interval)
-    market_sentiment = get_market_sentiment(symbol, interval, rsi_time_period)
+def print_results():
+    symbol = 'BTCUSDT'
+    interval = '1d'
+    rsi_time_period = 14
 
-    print(f'Part 1 - RSI Values:\n{rsi_value}')
-    print(f'Part 2 - Market Trend:\n{market_trend}')
-    print(f'Part 3 - Market Sentiment:\n{market_sentiment}')
+    # get RSI from part 1
+    rsi = get_rsi(symbol, interval, rsi_time_period)
+    print(f'RSI: {rsi}')
+
+    # get market trend from part 2
+    market_trend = get_market_trend(symbol, interval)
+    print(f'Market Trend: {market_trend}')
+
+    # get market sentiment from part 3
+    market_sentiment = get_market_sentiment(symbol, interval, rsi_time_period)
+    print(f'Market Sentiment: {market_sentiment}')
 
 print_results()
-
