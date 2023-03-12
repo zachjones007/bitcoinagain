6faa69f5e78d27c4e7ceae4a43b5e3f639182c72
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2023-03-12 12:43:28 -0700

    Update part3.py

--- part3.py
+++ part3.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import requests
 import numpy as np
 import pandas as pd
 
@@ -16,24 +17,29 @@ def get_market_sentiment(symbol, interval='1d', rsi_time_period=14):
     rsi_value = get_rsi(symbol, interval, rsi_time_period)
     market_trend = get_market_trend(symbol, interval)
     if market_trend == 'Bullish':
-        market_trend_value = 1
+        sentiment = 1
     elif market_trend == 'Bearish':
-        market_trend_value = -1
+        sentiment = -1
     else:
-        market_trend_value = 0
-        
-    # calculate the 20-day moving average
-    df = pd.read_csv(f"{symbol}_{interval}.csv")
-    df['SMA_20'] = df['Close'].rolling(window=20).mean()
-    latest_close = df.iloc[-1]['Close']
-    latest_sma = df.iloc[-1]['SMA_20']
-    if latest_close > latest_sma:
-        sma_trend_value = 1
+        if rsi_value < 30:
+            sentiment = 1
+        elif rsi_value > 70:
+            sentiment = -1
+        else:
+            sentiment = 0
+
+    return sentiment, rsi_value
+
+if __name__ == '__main__':
+    symbol = 'bitcoin'
+    interval = 'daily'
+    rsi_time_period = 14
+    market_sentiment, rsi_value = get_market_sentiment(symbol, interval, rsi_time_period)
+
+    if market_sentiment == 1:
+        print('Market sentiment is bullish with RSI value of', rsi_value)
+    elif market_sentiment == -1:
+        print('Market sentiment is bearish with RSI value of', rsi_value)
     else:
-        sma_trend_value = -1
-
-    # calculate the market sentiment score as the sum of RSI and trend values
-    market_sentiment = rsi_value + market_trend_value + sma_trend_value
-
-    return market_sentiment
+        print('Market sentiment is neutral with RSI value of', rsi_value)
 
