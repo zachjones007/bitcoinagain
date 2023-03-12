482dd8b797fcb999ea4bbba69401b80c4db776d3
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2023-03-12 13:10:44 -0700

    Update part2.py

--- part2.py
+++ part2.py
@@ -1,44 +1,14 @@
-import requests
+from get_market_data import get_price, get_volume
 
-def get_market_sentiment(symbol, interval='1d', rsi_time_period=14):
-    # Get RSI
-    url = f'https://api.coingecko.com/api/v3/coins/{symbol}/market_chart?vs_currency=usd&days=30&interval={interval}'
-    response = requests.get(url)
-    data = response.json()
-
-    closes = [d[1] for d in data['prices']]
-    rsi_values = []
-    for i in range(rsi_time_period, len(closes)+1):
-        delta = np.diff(closes[i-rsi_time_period:i])
-        ups = delta.clip(min=0)
-        downs = -1*delta.clip(max=0)
-        ups_avg = np.mean(ups)
-        downs_avg = np.mean(downs)
-        rs = ups_avg / downs_avg
-        rsi = 100 - (100 / (1 + rs))
-        rsi_values.append(rsi)
-
-    rsi_value = rsi_values[-1]
-
-    # Get Trading Volume
-    url = f'https://api.coingecko.com/api/v3/coins/{symbol}/market_chart?vs_currency=usd&days=30&interval={interval}'
-    response = requests.get(url)
-    data = response.json()
-
-    volumes = [d[1] for d in data['total_volumes']]
-    volume_score = ((volumes[-1] - min(volumes)) / (max(volumes) - min(volumes))) * 100
-
-    # Determine Market Sentiment
-    if rsi_value < 30:
-        sentiment = 'Strongly Bullish'
-    elif rsi_value < 50:
-        sentiment = 'Bullish'
-    elif rsi_value < 70:
-        sentiment = 'Neutral'
-    elif rsi_value < 90:
-        sentiment = 'Bearish'
+def make_trade():
+    price = get_price()
+    volume = get_volume()
+    if price < 50000 and volume > 100000000:
+        return 'buy'
+    elif price > 55000:
+        return 'sell'
     else:
-        sentiment = 'Strongly Bearish'
+        return 'hold'
 
-    print(f'Market Sentiment: {sentiment}, Volume Score: {volume_score:.2f}')
-    return sentiment, volume_score
+if __name__ == '__main__':
+    print(f"Trade decision: {make_trade()}")
