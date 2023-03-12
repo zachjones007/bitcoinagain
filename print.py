1d5ca88d6af1c9c9e0a47ff5b702025d7ac02582
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2023-03-12 10:59:23 -0700

    Update print.py

--- print.py
+++ print.py
@@ -1,24 +1,24 @@
-def print_results():
-    symbol = 'BTCUSDT'
-    interval = '1d'
-    rsi_time_period = 14
+import os
+import sys
 
-    # Part 1 - RSI Values
-    rsi_value = get_rsi(symbol, interval, rsi_time_period)
-    print('Part 1 - RSI Values:')
-    print(rsi_value)
+# add the bitcoinagain directory to the path
+module_path = os.path.abspath(os.path.join('..'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
 
-    # Part 2 - Overbought/Oversold Values
-    overbought_oversold = get_overbought_oversold(symbol, interval)
-    print('Part 2 - Overbought/Oversold Values:')
-    if overbought_oversold == "Overbought":
-        print("Overbought")
-    elif overbought_oversold == "Oversold":
-        print("Oversold")
-    else:
-        print("Neutral")
+# import the part3 function
+from bitcoinagain.part3 import get_market_sentiment
 
-    # Part 3 - Analysis Result
-    market_sentiment = get_market_sentiment(symbol, interval, rsi_time_period)
-    print('Part 3 - Analysis Result:')
-    print(market_sentiment)
+symbol = 'BTCUSDT'
+interval = '1d'
+rsi_time_period = 14
+
+market_sentiment = get_market_sentiment(symbol, interval, rsi_time_period)
+
+if market_sentiment < 30:
+    print('Market Sentiment: 0 (Bearish)')
+elif market_sentiment > 70:
+    print('Market Sentiment: 100 (Bullish)')
+else:
+    print(f'Market Sentiment: {market_sentiment:.2f}')
+
